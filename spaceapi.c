818536e6f27c1106d5f71d6f40ad3bc855993fed
Robert Heumueller <robert@heum.de>
2013-08-15 18:38:58 +0200

    Refactored curl request method

--- spaceapi.c
+++ spaceapi.c
@@ -9,17 +9,17 @@ CURLcode res;
 
 void init_icons(){
     GdkPixbufLoader *loader;
-    
+
     loader = gdk_pixbuf_loader_new();
     gdk_pixbuf_loader_write(loader, green_png, 3028, NULL);
     green_icon = gdk_pixbuf_loader_get_pixbuf(loader);
     gdk_pixbuf_loader_close(loader, NULL);  
-    
+
     loader = gdk_pixbuf_loader_new();
     gdk_pixbuf_loader_write(loader, yellow_png, 3120, NULL);
     yellow_icon = gdk_pixbuf_loader_get_pixbuf(loader);
     gdk_pixbuf_loader_close(loader, NULL);  
-    
+
     loader = gdk_pixbuf_loader_new();
     gdk_pixbuf_loader_write(loader, red_png, 3026, NULL);
     red_icon = gdk_pixbuf_loader_get_pixbuf(loader); 
@@ -59,20 +59,24 @@ size_t allocate_buffer(void *ptr, size_t size, size_t nmemb, void *data){
     return count;
 }
 
-gboolean fetch_directory(gpointer data){
-    char *buf = NULL;
+char* perform_http_get(const char* url){
+    char* buf = NULL;
     curl = curl_easy_init();
-    const char* url = "http://spaceapi.net/directory.json";
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, allocate_buffer);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buf);
     res = curl_easy_perform(curl);
     if(res != CURLE_OK){
-      fputs("curl failed", stderr);
-    }else{
-        puts(buf);
+        fputs("curl failed", stderr);
     }
     curl_easy_cleanup(curl);
+    return buf;
+}
+
+gboolean fetch_directory(gpointer data){
+    char *buf = NULL;
+    buf = perform_http_get("http://spaceapi.net/directory.json");
+    puts(buf);
     free(buf);
     return FALSE;
 }
@@ -85,6 +89,6 @@ int main(int argc, char **argv){
 
     g_idle_add(fetch_directory, NULL);
     g_timeout_add(500, do_something, status_icon);
-    
+
     gtk_main();
 }
