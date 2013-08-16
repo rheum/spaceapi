280c2f13a0bcc9175bd0fbef68722123c90627a7
Robert Heumueller <robert@heum.de>
2013-08-17 00:11:15 +0200

    Populating context menu

--- spaceapi.c
+++ spaceapi.c
@@ -1,48 +1,71 @@
 #include "spaceapi.h"
 
-GdkPixbuf *green_icon;
-GdkPixbuf *yellow_icon;
-GdkPixbuf *red_icon;
+GdkPixbuf *green_icon = NULL;
+GdkPixbuf *yellow_icon = NULL;
+GdkPixbuf *red_icon = NULL;
 
-CURL *curl;
-CURLcode res;
+GtkStatusIcon *status_icon = NULL;
+GtkMenu *menu = NULL;
+GtkMenu *sub_menu = NULL;
+GtkCheckMenuItem** space_items = NULL;
 
 struct hacker_space *directory = NULL;
 int space_count = 0;
 
-void init_icons(){
+void init_icons() {
     GdkPixbufLoader *loader;
 
     loader = gdk_pixbuf_loader_new();
     gdk_pixbuf_loader_write(loader, green_png, 3028, NULL);
     green_icon = gdk_pixbuf_loader_get_pixbuf(loader);
-    gdk_pixbuf_loader_close(loader, NULL);  
+    gdk_pixbuf_loader_close(loader, NULL);
 
     loader = gdk_pixbuf_loader_new();
     gdk_pixbuf_loader_write(loader, yellow_png, 3120, NULL);
     yellow_icon = gdk_pixbuf_loader_get_pixbuf(loader);
-    gdk_pixbuf_loader_close(loader, NULL);  
+    gdk_pixbuf_loader_close(loader, NULL);
 
     loader = gdk_pixbuf_loader_new();
     gdk_pixbuf_loader_write(loader, red_png, 3026, NULL);
-    red_icon = gdk_pixbuf_loader_get_pixbuf(loader); 
-    gdk_pixbuf_loader_close(loader, NULL);  
+    red_icon = gdk_pixbuf_loader_get_pixbuf(loader);
+    gdk_pixbuf_loader_close(loader, NULL);
 
-    if (green_icon == NULL || yellow_icon == NULL || red_icon == NULL){
+    if (green_icon == NULL || yellow_icon == NULL || red_icon == NULL) {
         fputs("Failed to load icons from resources.", stderr);
         exit(1);
     }
 }
 
-gboolean do_something(gpointer data){
+void init_gui() {
+    menu = (GtkMenu*) gtk_menu_new();
+    sub_menu = (GtkMenu*) gtk_menu_new();
+    gtk_widget_set_sensitive((GtkWidget*) sub_menu, FALSE);
+
+    GtkMenuItem *sub_menu_item = (GtkMenuItem*) gtk_menu_item_new_with_label("Spaces");
+    gtk_menu_item_set_submenu(sub_menu_item, (GtkWidget*) sub_menu);
+
+    GtkImageMenuItem *quit_menu_item = (GtkImageMenuItem*)
+                                       gtk_image_menu_item_new_from_stock(GTK_STOCK_QUIT, NULL);
+    g_signal_connect(G_OBJECT(quit_menu_item), "activate",
+                     G_CALLBACK(gtk_main_quit), NULL);
+
+    GtkImageMenuItem *settings_menu_item = (GtkImageMenuItem*)
+                                           gtk_image_menu_item_new_from_stock(GTK_STOCK_PREFERENCES, NULL);
+
+    gtk_menu_shell_append((GtkMenuShell*) menu, (GtkWidget*) sub_menu_item);
+    gtk_menu_shell_append((GtkMenuShell*) menu, (GtkWidget*) settings_menu_item);
+    gtk_menu_shell_append((GtkMenuShell*) menu, (GtkWidget*) quit_menu_item);
+}
+
+gboolean do_something(gpointer data) {
     static int i = 0;
-    if (i == 0){
+    if (i == 0) {
         gtk_status_icon_set_from_pixbuf(data, green_icon);
     }
-    else if (i == 1){
+    else if (i == 1) {
         gtk_status_icon_set_from_pixbuf(data, yellow_icon);
     }
-    else if (i == 2){
+    else if (i == 2) {
         gtk_status_icon_set_from_pixbuf(data, red_icon);
     }
     i++;
@@ -50,58 +73,27 @@ gboolean do_something(gpointer data){
     return TRUE;
 }
 
-size_t allocate_buffer(void *ptr, size_t size, size_t nmemb, void *data){
-    size_t count = (size_t)(size*nmemb);
-    char** buf = ((char **) data);
-    if (*buf == NULL){
-        *buf = strndup(ptr, count); 
-    }else {
-        *buf = realloc(*buf, (size_t) (strlen(*buf) + count + 1));
-        strncat(*buf, ptr, count);
-    }
-    return count;
+gboolean update_directory(gpointer data) {
+    if (fetch_directory())
+        update_menu_items();
+    return FALSE;
 }
 
-char* perform_http_get(const char* url){
-    char* buf = NULL;
-    curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, allocate_buffer);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buf);
-    res = curl_easy_perform(curl);
-    if(res != CURLE_OK){
-        fputs("curl failed", stderr);
-        buf = NULL;
-    }
-    curl_easy_cleanup(curl);
-    return buf;
-}
-
-void free_directory(){
-    for (int i = 0; i < space_count; i++){
-        free(directory[i].name);
-        free(directory[i].url);
-    }
-    free(directory);
-    directory = NULL;
-    space_count = 0;
-}
-
-gboolean fetch_directory(gpointer data){
-    if (directory != NULL){
+gboolean fetch_directory() {
+    if (directory != NULL) {
         free_directory();
     }
 
     char *buf = NULL;
-    buf = perform_http_get("http://spaceapi.net/directory.json");
+    buf = perform_http_get(DIRECTORY_URL);
 
     if (buf == NULL)
-        return TRUE;
+        return FALSE;
 
     char *ptr = strtok(buf, "{}\t\n");
     char *ptr2 = NULL;
     char *ptr3 = NULL;
-    for (; ptr != NULL; ptr = strtok(NULL, "{}\t\n")){
+    for (; ptr != NULL; ptr = strtok(NULL, "{}\t\n")) {
         ptr = strchr(ptr, '"');
         ptr2 = strchr(ptr, ':');
         ptr3 = strchr(ptr2 + 2, '"');
@@ -111,21 +103,59 @@ gboolean fetch_directory(gpointer data){
         space.url = strndup(ptr2 + 2, ptr3 - ptr2 -2);
 
         space_count++;
-        directory = realloc(directory, 
-                space_count * sizeof(struct hacker_space));
+        directory = realloc(directory,
+                            space_count * sizeof(struct hacker_space));
         directory[space_count-1] = space;
     }
     free(buf);
+    qsort(directory, space_count, sizeof(struct hacker_space), compare_spaces);
     return TRUE;
 }
 
-int main(int argc, char **argv){
+void free_directory() {
+    gtk_widget_set_sensitive((GtkWidget*) sub_menu, FALSE);
+    for (int i = 0; i < space_count; i++) {
+        free(directory[i].name);
+        free(directory[i].url);
+        gtk_widget_destroy((GtkWidget*) space_items[i]);
+    }
+    free(directory);
+    directory = NULL;
+    space_count = 0;
+}
+
+static int compare_spaces(const void *p1, const void *p2) {
+    return strcmp(((struct hacker_space*) p1)->name, ((struct hacker_space*) p2)->name);
+}
+
+void update_menu_items() {
+    space_items = realloc(space_items, space_count * sizeof(GtkCheckMenuItem));
+    for (int i = 0; i < space_count; i++) {
+        space_items[i] = (GtkCheckMenuItem*) gtk_check_menu_item_new_with_label(directory[i].name);
+        gtk_menu_shell_append((GtkMenuShell*) sub_menu, (GtkWidget*) space_items[i]);
+    }
+    gtk_widget_set_sensitive((GtkWidget*) sub_menu, TRUE);
+}
+
+
+static void popup_menu(GtkStatusIcon *status_icon, guint button,
+                       guint activate_time, gpointer user_data) {
+    gtk_widget_show((GtkWidget*) menu);
+    gtk_widget_show_all((GtkWidget*) menu);
+    gtk_menu_popup(menu, NULL, NULL, gtk_status_icon_position_menu,
+                   status_icon, button, activate_time);
+}
+
+
+int main(int argc, char **argv) {
     gtk_init(&argc, &argv);
     init_icons();
+    init_gui();
 
-    GtkStatusIcon *status_icon = gtk_status_icon_new_from_pixbuf(green_icon);
+    status_icon = gtk_status_icon_new_from_pixbuf(green_icon);
+    g_signal_connect(G_OBJECT(status_icon), "popup-menu", G_CALLBACK(popup_menu), NULL);
 
-    g_idle_add(fetch_directory, NULL);
+    g_idle_add(update_directory, NULL);
     g_timeout_add(500, do_something, status_icon);
 
     gtk_main();
--- spaceapi.h
+++ spaceapi.h
@@ -1,13 +1,33 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <curl/curl.h>
 #include <string.h>
+#include "http.h"
 #include "green.h"
 #include "yellow.h"
 #include "red.h"
+
+#define DIRECTORY_URL "http://spaceapi.net/directory.json"
 
 struct hacker_space {
     char* name;
     char* url;
 };
+
+
+void init_icons();
+
+void init_gui();
+
+gboolean update_directory();
+
+gboolean fetch_directory();
+
+static int compare_spaces(const void *p1, const void *p2);
+
+void free_directory();
+
+void update_menu_items();
+
+static void popup_menu(GtkStatusIcon *status_icon, guint button,
+        guint activate_time, gpointer user_data);
